e44f6de41afa4ebf4623d30fb734f19adcdbd2f9
WYQ <wyqpadding@gmail.com>
2022-03-10 15:56:13 +0800

    update rb

--- Classes/remove_kingfisher_swiftui.rb
+++ Classes/remove_kingfisher_swiftui.rb
@@ -1,9 +1,11 @@
 # 解决 xcode13 Kingfisher Release模式下SwiftUI报错问题
 puts "-------------------->>>"
-system("pwd")
+dir = system("pwd")
+puts dir
 system("ls Pods/Kingfisher/Sources/General")
 system("rm -rf Pods/Kingfisher/Sources/SwiftUI")
-code_file = "Pods/Kingfisher/Sources/General/KFOptionsSetter.swift"
+# files = Dir.entries(dir + "Pods/Kingfisher/Sources")
+code_file = dir + "Pods/Kingfisher/Sources/General/KFOptionsSetter.swift"
 code_text = File.read(code_file)
 code_text.gsub!(/#if canImport\(SwiftUI\) \&\& canImport\(Combine\)(.|\n)+#endif/,'')
 system("rm -rf " + code_file)
