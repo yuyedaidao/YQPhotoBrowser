1f1986db4c44ff7a29c43d63a54d9aedfc4e2c43
WYQ <wyqpadding@gmail.com>
2022-03-10 16:05:59 +0800

    update rb

--- Classes/remove_kingfisher_swiftui.rb
+++ Classes/remove_kingfisher_swiftui.rb
@@ -6,9 +6,11 @@ system("ls Pods/Kingfisher/Sources/General")
 system("rm -rf Pods/Kingfisher/Sources/SwiftUI")
 # files = Dir.entries(dir + "Pods/Kingfisher/Sources")
 code_file = dir + "Pods/Kingfisher/Sources/General/KFOptionsSetter.swift"
-code_text = File.read(code_file)
-code_text.gsub!(/#if canImport\(SwiftUI\) \&\& canImport\(Combine\)(.|\n)+#endif/,'')
-system("rm -rf " + code_file)
-aFile = File.new(code_file, 'w+')
-aFile.syswrite(code_text)
-aFile.close()
+if File.exist?(code_file) then
+    code_text = File.read(code_file)
+    code_text.gsub!(/#if canImport\(SwiftUI\) \&\& canImport\(Combine\)(.|\n)+#endif/,'')
+    system("rm -rf " + code_file)
+    aFile = File.new(code_file, 'w+')
+    aFile.syswrite(code_text)
+    aFile.close()
+end
